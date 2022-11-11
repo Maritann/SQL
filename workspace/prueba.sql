81e77d57651f7dd2e3abe7df9f27ae6aed86efd1
Maritann <cristina.tnk.44@gmail.com>
2022-11-10 23:26:01 -0300

    prueba SQL

---
+++ workspace/prueba.sql
@@ -0,0 +1,41 @@
+CREATE TABLE ejemplo.usuario  (
+	id INT NOT NULL,
+	nombre varchar(100) NOT NULL,
+	apellido varchar(100) NOT null,
+	rut varchar(100) NOT null,
+	telefono varchar(100) NOT null,
+	correo varchar(100) NOT null,
+	prevision varchar(100) NOT null,
+	edad float not null,
+	rol varchar(100) NOT null,
+	genero varchar(100)
+);
+
+
+CREATE TABLE ejemplo.paciente  (
+		id INT NOT NULL,
+	nombre varchar(100) NOT NULL,
+	apellido varchar(100) NOT null,
+	rut varchar(100) NOT null,
+	telefono varchar(100) NOT null,
+	correo varchar(100) NOT null,
+	prevision varchar(100) NOT null,
+	edad float not null,
+	rol varchar(100) NOT null,
+	genero varchar(100)
+
+);
+
+CREATE TABLE ejemplo.especialista  (
+		id INT NOT NULL,
+	nombre varchar(100) NOT NULL,
+	apellido varchar(100) NOT null,
+	rut varchar(100) NOT null,
+	telefono varchar(100) NOT null,
+	correo varchar(100) NOT null,
+	prevision varchar(100) NOT null,
+	edad float not null,
+	rol varchar(100) NOT null,
+	genero varchar(100)
+
+);
